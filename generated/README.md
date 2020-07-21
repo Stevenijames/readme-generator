6d13b635d7208235af1377865170600a78a005a5
Steven Ijames <ijamesproductions1@gmail.com>
2020-07-20 22:51:42 -0600

    updates to index.js, more questions

---
+++ generated/README.md
@@ -0,0 +1,29 @@
+# Good Readme
+    
+    # Project Name
+    Good Readme
+
+    ## Description
+    building a great README.md for project
+
+    ## Table of Contents
+    ajskdlfa
+
+    ## Installation
+    ajsdkflad
+
+    ## Usage
+    aosdijfso
+
+    ## License
+    oasdijfsaod
+
+    ## Contributing
+    absdklafs
+
+    ## Tests
+    ahsdlfasdf
+    
+    ## Questions?
+    asbdjfksadf
+    
--- index.js
+++ index.js
@@ -1,52 +1,87 @@
-// creates a README file
-// writing to the README file
-// ask user some questions
-// based on answers will generate a README file
-
 const inquirer = require("inquirer");
 const fs = require("fs");
-// const num = ["x"+ "y"];
-// num.push(2);
-//ask user questions
+
 function askQuestion() {
     return inquirer.prompt([{
         type: "input",
-        message: "What is your projects name?",
+        message: "What is your project's name?",
         name: "projectName"
     }, {
         type: "input",
-        message: "What is your projects link?",
-        name: "projectLink"
-    }]);
+        message: "What is your project's description?",
+        name: "projectDescription"
+    }, {
+        type: "input",
+        message: "What is your project's table of Contents?",
+        name: "projectTableofContents"
+    }, {
+        type: "input",
+        message: "Is there an Installation needed?",
+        name: "projectInstallation"
+    }, {
+        type: "input",
+        message: "Usage?",
+        name: "projectUsage"
+    }, {
+        type: "input",
+        message: "License?",
+        name: "projectLicense"
+    }, {
+        type: "input",
+        message: "Contributing?",
+        name: "projectContributing"
+    }, {
+        type: "input",
+        message: "Tests?",
+        name: "projectTests"
+    }, {
+        type: "input",
+        message: "Questions?",
+        name: "projectQuestions"
+    }
+
+    ]);
 }
 
 function generateReadme(response) {
     return `# ${response.projectName}
     
+    # Project Name
+    ${response.projectName}
+
     ## Description
-    ${response.projectLink}
+    ${response.projectDescription}
 
+    ## Table of Contents
+    ${response.projectTableofContents}
 
-# DU - project - 1
-# Elifino - City Search, Maps and Language Translator
-    GIVEN a city search dashboard with a form input
-    WHEN I search for a city
-THEN I am presented with city weather, map and language translation for that city.That city is added to the search history
-        * Search Bar`
+    ## Installation
+    ${response.projectInstallation}
+
+    ## Usage
+    ${response.projectUsage}
+
+    ## License
+    ${response.projectLicense}
+
+    ## Contributing
+    ${response.projectContributing}
+
+    ## Tests
+    ${response.projectTests}
+    
+    ## Questions?
+    ${response.projectQuestions}
+    `
 }
 
-
-// saveReadme(response)
-// saves README to a file, how do I save a file??? Is that save place in the browser???
 function saveReadme(readmeData) {
-    fs.writeFile('README.md', readmeData, function (err) {
+    fs.writeFile('generated/README.md', readmeData, function (err) {
         if (err) throw err;
         console.log('The file has been saved!');
     })
 }
 
-
-// initializing the script 
 async function init(reponse) {
     const answers = await askQuestion();
     const text = generateReadme(answers);
